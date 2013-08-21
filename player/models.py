933ed1e781d28aff154d6f77c2282c1020091d76
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-08-21 09:42:34 +0200

    Added metadata parsing (with mutagen)

--- player/models.py
+++ player/models.py
@@ -9,12 +9,8 @@ class Song(models.Model):
     artist = models.CharField(_('Artist'), max_length=100, blank=True)
     album = models.CharField(_('Album name'), max_length=100, blank=True)
     track_number = models.SmallIntegerField(_('Track number'), blank=True, null=True)
-    track_total = models.SmallIntegerField(_('Total track count'), blank=True, null=True)
     genre = models.CharField(_('Genre'), max_length=50, blank=True)
-    disc_number = models.SmallIntegerField(_('Disc number'), blank=True, null=True)
-    disc_total = models.SmallIntegerField(_('Total disc count'), blank=True, null=True)
     year = models.CharField(_('Year'), max_length=4, blank=True)
-    publisher = models.CharField(_('Publisher'), max_length=100, blank=True)
     play_count = models.IntegerField(_('Play count'), default=0)
 
     def save(self, *args, **kwargs):
