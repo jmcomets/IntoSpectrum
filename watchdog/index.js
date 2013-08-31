dbdd2631c0996ee19fb22298d515c0def514717d
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-08-31 17:44:08 +0200

    Configuring fixed, wait and interval done

--- watchdog/index.js
+++ watchdog/index.js
@@ -1,53 +1,92 @@
-var path = require('path');
+var path = require('path'),
+    fs = require('fs');
+
+// Walk through a directory (parallel loop), found on SO:
+//  >> http://stackoverflow.com/a/5827895/1441984
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = path.join(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
 
 // Options for watchdog
 var options = {
-  interval: {
-    hours: 24,
-    minutes: 0,
-    seconds: 0
-  }, delay: 0
+  'interval': {
+    'hours': 24,
+    'minutes': 0,
+    'seconds': 0
+  }, 'delay': 0
 };
 
 // Configuration via options
 var configure = exports.configure = function(opts) {
-  options.interval.seconds = opts.interval.seconds;
-  options.interval.minutes = opts.interval.minutes;
-  options.interval.hours = opts.interval.hours;
-  options.delay = opts.delay || 0;
+  if (opts.interval != undefined) { options.interval = opts.interval; }
+  if (opts.delay != undefined) { options.delay = opts.delay; }
 }
 
 // Callbacks (exported via on)
 var callbacks = {
-  started: function() {},
-  finished: function() {}
+  'started': function() {},
+  'finished': function() {}
 };
 // callbacks configuring
 exports.on = function(evt, fn) { callbacks.evt = fn; };
 
-var intervalId = 0;
+// Internal ID of the current started
+var intervalId = null,
+    started = false;
 
 // Start watchdog, reapplying all options
 exports.start = function(opts) {
-  // First of all, configure if extra options are given
+  // Fail if already started
+  if (started) { throw new Error('Watchdog already started'); }
+
+  // Configure if extra options are given
   if (opts) { configure(opts); }
   var seconds = options.interval.seconds || 0,
       minutes = options.interval.minutes || 0,
       hours = options.interval.hours || 0,
-      delay = options.delay || 0;
+      delay = options.delay || 0,
+      total_seconds = seconds + 60*(minutes + 60*hours);
 
-  // TODO implement initial wait
-  console.log('Configuring with dummy startup delay (' + delay + ')');
+  // Log configuration
+  console.log('Starting watchdog: will run every ' + total_seconds
+      + ' seconds, starting after ' + delay + ' seconds.');
 
-  // Now schedule the working process
-  var total_seconds = seconds + 60*(minutes + 60*hours);
-  console.log('Configuring with repeat interval = ' + total_seconds + ' seconds');
-  intervalId = setInterval(function() {
-    console.log('Working'); // TODO
-  }, total_seconds*1000);
+  // Initial wait
+  started = true;
+  setTimeout(function() {
+    var worker = function() {
+      console.log('Working'); // TODO
+    };
+    worker();
+    intervalId = setInterval(worker, 1000*total_seconds);
+  }, 1000*delay);
 }
 
 // Stop watchdog
-exports.stop = function() { clearInterval(intervalId); }
+exports.stop = function() {
+  if (started == false) { throw new Error('Watchdog not started'); }
+  if (intervalId != null) { clearInterval(intervalId); }
+  started = false;
+  intervalId = null;
+}
 
 // vim: ft=javascript et sw=2 sts=2
