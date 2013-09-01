3a1150a0e6ce947e4c7d7035b05b9c5e86778f33
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-01 18:11:45 +0200

    Separated get logic from process logic

--- static/js/base.js
+++ static/js/base.js
@@ -1,16 +1,19 @@
 $(window).load(function() {
-  var loadSongs = function(c) {
-    var cursor = parseInt(c) || 1;
-    $.getJSON('/library/' + cursor, function(data) {
-      console.log(data);
-      if (data.next) {
-        loadSongs(cursor + 1);
-      }
-    });
-  };
-
   // Load songs
-  loadSongs();
+  (function(fn) {
+    var loadSongs = function(c) {
+      var cursor = parseInt(c) || 1;
+      $.getJSON('/library/' + cursor, function(data) {
+        if (data.next) {
+          loadSongs(cursor + 1);
+          fn(data.songs);
+        }
+      });
+    };
+    loadSongs();
+  })(function(songs) {
+    console.log(songs); // TODO
+  });
 
   // Main player instance
   var player = new Player('/player');
