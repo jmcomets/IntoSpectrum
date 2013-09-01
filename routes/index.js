3e9917793dd2fd64e5751ce0ee33ed33226f9794
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-01 17:47:26 +0200

    Removed logs, fixed end criteria

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,13 @@ exports.library = function(req, res) {
   var cursor = (parseInt(req.params.cursor) || 1) - 1,
       limit = 100,
       offset = limit*cursor;
-  console.log(cursor);
-  console.log(limit);
-  console.log(offset);
   songs = Song.findAll({
     'offset': offset,
     'limit': limit
   }).success(function(songs) {
     res.json({
       'songs': songs,
-      'next': songs.length > 0
+      'next': songs.length == limit
     });
   });
 };
