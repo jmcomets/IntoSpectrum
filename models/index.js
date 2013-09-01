3ac14c793147d77619536a13fdaeaeb02433c863
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-01 18:02:54 +0200

    Layout fixed, added paginated loading of song database

---
+++ models/index.js
@@ -0,0 +1,40 @@
+var Sequelize = require('sequelize'),
+    db = new Sequelize('into_spectrum', 'root', '', {
+      'dialect': 'mysql',
+      'logging': false,
+      'sync': {
+        'force': true
+      }
+    });
+
+var Song = exports.Song = db.define('Song', {
+  'id': {
+    'type': Sequelize.INTEGER,
+    'primaryKey': true,
+    'autoIncrement': true
+  },
+  'path': {
+    'type': Sequelize.STRING,
+    'unique': true
+  },
+  'title': {
+    'type': Sequelize.STRING
+  },
+  'artist': {
+    'type': Sequelize.STRING
+  },
+  'album': {
+    'type': Sequelize.STRING
+  },
+  'year': {
+    'type': Sequelize.INTEGER
+  },
+  'play_count': {
+    'type': Sequelize.INTEGER,
+    'defaultValue': 0
+  }
+}, {
+  'tableName': 'songs'
+});
+
+Song.sync();
--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,28 @@
-// Our player instance
-var player = new Player('/player');
+$(window).load(function() {
+  var loadSongs = function(c) {
+    var cursor = parseInt(c) || 1;
+    $.getJSON('/library/' + cursor, function(data) {
+      console.log(data);
+      if (data.next) {
+        loadSongs(cursor + 1);
+      }
+    });
+  };
 
-// Event hooks
-player.on('play', function() {
+  // Load songs
+  loadSongs();
+
+  // Main player instance
+  var player = new Player('/player');
+
+  // Player event hooks
+  player.on('play', function() {
     console.log('play event');
-}).on('pause', function() {
+  }).on('pause', function() {
     console.log('pause event');
-}).on('stop', function() {
+  }).on('stop', function() {
     console.log('stop event');
+  });
 });
+
+// vim: ft=javascript et sw=2 sts=2
