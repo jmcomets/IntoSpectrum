ecb9747d651fb864b1747cfbb63aa9e1e487a07f
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-09-06 14:12:29 +0200

    Slow down the emitter

--- network/player.js
+++ network/player.js
@@ -9,35 +9,43 @@ exports.listen = function(server) {
   var io = SocketIO.listen(server).of(settings.player.url);
   io.on('connection', function(socket) {
     socket.on('get_info', function() {
-      player.send_info();
+      send_info();
     });
     socket.on('play', function(id) {
       player.play(id);
+      send_info();
     });
     socket.on('pause', function(id) {
       player.pause(id);
+      send_info();
     });
     socket.on('unpause', function(id) {
       player.unpause(id);
+      send_info();
     });
     socket.on('stop', function(id) {
       player.stop(id);
+      send_info();
     });
     socket.on('volume', function(volume) {
       player.volume(volume);
+      send_info();
     });
     socket.on('time', function(id, time) {
       player.time(id, time);
+      send_info();
     });
   });
 
-  // Timer
-  var delay = 420;
-  var timer = setInterval(function() {
+  var send_info = function() {
     var info = player.get_info();
     console.log(info);
     io.emit('info', info);
-  }, delay);
+  };
+
+  // Timer
+  var delay = 5000;
+  var timer = setInterval(send_info, delay);
 };
 
 // vim: ft=javascript et sw=2 sts=2
