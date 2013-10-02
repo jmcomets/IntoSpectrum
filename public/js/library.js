f5cea6b70c1f7a26bad1d5864f97f4fd0d581aba
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-03 00:20:54 +0200

    More work on library loading

--- public/js/library.js
+++ public/js/library.js
@@ -1,6 +1,9 @@
 function LibraryCtrl($scope, $http) {
-  // Flag indicating if the library is loading
-  $scope.loading = true;
+  // Loader: handles loading state
+  $scope.loader = {
+    percentage: 0,
+    running: true
+  };
 
   // List of song models
   $scope.songs = [];
@@ -8,30 +11,24 @@ function LibraryCtrl($scope, $http) {
   // Library initialization
   $scope.init = function() {
     // Load songs protocol (data handler, current cursor, end handler)
-    var loadSongs = function(fn, end, c) {
+    var loadSongs = function(c) {
       var cursor = parseInt(c, 10) || 1;
       $http({
-        'method': 'GET',
-        'url': '/library/' + cursor
+        method: 'GET',
+        url: '/library/' + cursor
       }).success(function(data) {
-        fn(data);
-        if (data.next) {
-          loadSongs(fn, end, cursor + 1);
-        } else { end(); }
+        // Append songs to the library
+        $scope.songs = $scope.songs.concat(data.songs);
+
+        // Update load progress
+        $scope.loader.percentage = 100 * $scope.songs.length / data.count;
+
+        // Recursion for next load
+        if (data.next) { loadSongs(cursor + 1); }
+        else { $scope.loader.running = false; }
       });
     };
-
-    // Load songs
-    loadSongs(function(data) {
-      // Append songs to the library
-      $scope.songs = $scope.songs.concat(data.songs);
-
-      // Update load progress
-      // TODO
-    }, function() {
-      $scope.loading = false;
-      //$scope.$apply();
-    });
+    loadSongs();
   };
 
   // Play a song
@@ -41,24 +38,24 @@ function LibraryCtrl($scope, $http) {
 
   // Right click
   $scope.menu = {
-    'song': {},
-    'visible': false,
-    'top': 0, 'left': 0,
-    'open': function(evt, song) {
+    song: {},
+    visible: false,
+    top: 0, left: 0,
+    open: function(evt, song) {
       this.song = song;
       this.visible = true;
       this.top = evt.pageY;
       this.left = evt.pageX;
-    }, 'close': function() {
+    }, close: function() {
       this.visible = false;
       this.song = {};
-    }, 'play': function() {
+    }, play: function() {
       player.play(this.song.id);
       this.close();
-    }, 'pushQueue': function() {
+    }, pushQueue: function() {
       player.addToPlayQueue(this.song.id);
       this.close();
-    }, 'pushNext': function() {
+    }, pushNext: function() {
       player.addAsNext(this.song.id);
       this.close();
     }
