9b54581f6a36271eb3f560d85dd7854dc1fa2de2
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-04 10:16:34 +0200

    Fixed responsive mode for navbar

--- client/js/services/library.js
+++ client/js/services/library.js
@@ -3,13 +3,19 @@ app.service('$library', function($rootScope, $http, $q) {
     getSongs: function(c) {
       var cursor = parseInt(c) || 0, deferred = $q.defer();
       $http.get('/api/songs/' + cursor).success(function(data) {
-         $rootScope.$safeApply(function() {
-           deferred.resolve(data.songs);
-         });
+        angular.forEach(data.songs, function(song) {
+          song.title = (song.title) ? song.title : function(str) {
+            str = str.substring(str.lastIndexOf('/') + 1, str.length);
+            return str.substring(0, str.lastIndexOf('.'));
+          } (song.path);
+        });
+        $rootScope.$safeApply(function() {
+          deferred.resolve(data.songs);
+        });
       }).error(function() {
-         $rootScope.$safeApply(function() {
+        $rootScope.$safeApply(function() {
           deferred.error();
-         });
+        });
       });
       return deferred.promise;
     }
