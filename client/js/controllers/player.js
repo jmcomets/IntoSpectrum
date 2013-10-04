642de1300aceb897a3c1d954562eea40ef5f941b
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-04 09:05:41 +0200

    Added progress bar updating

--- client/js/controllers/player.js
+++ client/js/controllers/player.js
@@ -2,8 +2,6 @@ function PlayerCtrl($scope, $player) {
   $scope.random = true;
   $scope.playing = false;
   $scope.volume = 0;
-  $scope.currentTime = 0;
-  $scope.maxTime = 0;
 
   $scope.next = function() {
     if ($scope.playing == false) { $scope.playing = true; }
@@ -25,11 +23,28 @@ function PlayerCtrl($scope, $player) {
     // TODO actually change the random mode
   };
 
+  $scope.setTime = function(time) {
+    // Change time
+    $scope.time = time;
+
+    // Update every half-second
+    var secs = 1/2;
+
+    // Interval behaviour
+    if (this.id) { clearInterval(this.id); };
+    this.id = setInterval(function() {
+      $scope.$apply(function() {
+        $scope.time += secs;
+      });
+    }, 1000*secs);
+  };
+
   $player.bind('info', function(state) {
-    $scope.volume = this.state.volume;
     $scope.playing = this.state.playing;
-    $scope.currentTime = this.state.time;
-    $scope.maxTime = this.state.time_max;
     //$scope.random = this.state.random;
+    $scope.volume = parseFloat(this.state.volume);
+
+    $scope.setTime(parseFloat(this.state.time));
+    $scope.maxTime = parseFloat(this.state.time_max);
   });
 }
