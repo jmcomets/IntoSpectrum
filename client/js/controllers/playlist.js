3dacc6bccadbf25d406f5881d7780945114d1c3d
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-29 08:39:45 +0100

    Quick commit

--- client/js/controllers/playlist.js
+++ client/js/controllers/playlist.js
@@ -1,6 +1,14 @@
 function PlaylistCtrl($scope, $player) {
   $scope.playlist = [];
 
+  $scope.play = function($index) {
+    $player.play($scope.playlist[song]);
+    $scope.playlist.slice(1, $index);
+  };
+
+  $scope.remove = function($index) {
+  };
+
   $player.bind('info', function() {
     $scope.playlist = this.state.playlist;
     //console.log($scope.playlist);
