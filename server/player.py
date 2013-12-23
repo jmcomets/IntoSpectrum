a61858b35464eb72c48e2e4998e20ad63c2e3b1e
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-23 23:53:16 +0100

    Still not working...

--- server/player.py
+++ server/player.py
@@ -1,10 +1,27 @@
-from flask_socketio import FlaskNamespace
+import threading
 import mpd
+from flask_socketio import FlaskNamespace
 
 class PlayerNamespace(FlaskNamespace):
-    def on_info(self, msg):
-        print 'info:', msg
+    def __init__(self, *args, **kwargs):
+        super(PlayerNamespace, self).__init__(*args, **kwargs)
+        self.client = mpd.MPDClient()
+        self.client.connect('localhost', '6060')
+        self.client_lock = threading.Lock()
+
+    def __getattr__(self, attr):
+        actual_attr = super(PlayerNamespace, self).__getattr__(attr)
+        if attr.startswith('on_') and callable(actual_attr):
+            def inner(*args, **kwargs):
+                with self._client_lock:
+                    actual_attr(*args, **kwargs)
+            return inner
+        return actual_attr
+
     def on_next(self):
-        pass
+        with self.client_lock:
+            self.client.next()
+
     def on_previous(self):
-        pass
+        with self.client_lock:
+            self.client.previous()
