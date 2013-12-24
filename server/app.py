a0f806b41c2a7179718d2fabc095f0197cf6d44c
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-24 17:19:19 +0100

    Still not working...

--- server/app.py
+++ server/app.py
@@ -3,23 +3,19 @@ import sys
 from gevent import monkey; monkey.patch_all()
 from flask import Flask, request, jsonify, abort
 from socketio import socketio_manage
-import settings
-from player import Namespace, Client, database
-
-host, address = settings.FLASK_HOST, settings.FLASK_PORT
+from player import Namespace, database
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 public_dir = os.path.join(this_dir, '..', 'public')
 app = Flask('IntoSpectrum', static_folder=public_dir, static_url_path='')
-client = Client()
-
-@app.route('/socket.io/<path:path>')
-def run_socketio(path):
-    socketio_manage(request.environ, { '/player': Namespace })
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+@app.route('/socket.io/<path:path>')
+def run_socketio(path):
+    socketio_manage(request.environ, { '/player': Namespace })
 
 # song database api
 
@@ -35,7 +31,6 @@ def get_songs():
 
 @app.route('/api/songs/:song_id/')
 def find_song(song_id):
-    print song_id
     try:
         return jsonify(database.find_song_by_id(int(song_id)))
     except ValueError:
@@ -44,7 +39,9 @@ def find_song(song_id):
 if __name__ == '__main__':
     from werkzeug.wsgi import SharedDataMiddleware
     from socketio.server import SocketIOServer
-    print 'Listening on http://%s:%s' % (host, address)
+    import settings
+    host, address = settings.FLASK_HOST, settings.FLASK_PORT
+    print 'Server started on http://%s:%s' % (host, address)
     app.debug = '--debug' in sys.argv
     real_app = SharedDataMiddleware(app, { '/': public_dir })
     SocketIOServer((host, address), real_app, namespace='socket.io',
--- server/player/socket.py
+++ server/player/socket.py
@@ -4,28 +4,10 @@ from socketio.namespace import BaseNamespace
 from .client import Client
 
 class Namespace(BaseNamespace):
-    UPDATE_INTERVAL = 5
-    PROTOCOL = {
-            'status': 'info',
-            }
-
     def __init__(self, *args, **kwargs):
         super(Namespace, self).__init__(*args, **kwargs)
         self.client = Client()
         self.lock = threading.Lock()
-        self.status_thread = threading.Thread(target=self._send_status)
-        self.status_thread.daemon = True
-        self.status_thread.start()
-
-    def _send_status(self):
-        """
-        Status sending thread, emitting regularly mpd's status to all clients
-        (interval: UPDATE_INTERVAL).
-        """
-        while True:
-            with self.lock:
-                self.emit('status', self.client.status())
-            time.sleep(self.UPDATE_INTERVAL)
 
     def __getattribute__(self, attr):
         """
@@ -33,6 +15,7 @@ class Namespace(BaseNamespace):
         """
         actual_attr = super(Namespace, self).__getattribute__(attr)
         if attr.startswith('on_') and callable(actual_attr):
+            print 'event:', attr.replace('on_', '', 1)
             def inner(*args, **kwargs):
                 with self.lock:
                     retval = actual_attr(*args, **kwargs)
@@ -40,14 +23,5 @@ class Namespace(BaseNamespace):
             return inner
         return actual_attr
 
-    def emit(self, event, *args, **kwargs):
-        """
-        Overriden 'emit' method, applying the conversion from our protocol to
-        the client's protocol, allowing more flexibility between our naming
-        conventions and the client's conventions.
-        """
-        assert event in self.PROTOCOL, 'Unknown event "%s"' % event
-        return super(Namespace, self).emit(self.PROTOCOL[event], *args, **kwargs)
-
     def on_info(self):
-        self.emit('status', self.client.status())
+        self.emit('info', self.client.status())
